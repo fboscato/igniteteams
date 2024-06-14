a2f543ca64931f019ba0b5cee9e3755b8d7ecb43
fernandoboscato <fernandoboscato@faee.com.br>
2024-06-14 17:17:09 -0300

    Criado Botao

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,40 @@
-
+import { useState } from "react"
+import { FlatList } from "react-native"
 import { Text } from 'react-native';
 import { Container, } from './styles'
 import { Header } from '@/components/Header';
 import { Highlight } from '@/components/Highlight';
+import { GroupCard } from '@/components/CroupCard';
+import { ListEmpty } from "@/components/ListemEmpty";
+import { Button } from "@/components/Button";
 export function Groups() {
+  const [groups, setGroup] = useState([])
   return (
     <Container>
       <Header />
       <Highlight
-      title='Turma'
-      subtitle='Jogue com sua turma'
+        title='Turma'
+        subtitle='Jogue com sua turma'
       />
 
-    
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex:1}}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Nenhuma turma cadastrada"
+          />
+        )}
+      />
+      <Button
+        title="Criar nova turma"
+      />
     </Container>
   );
 }
